3c5e4e99f4e9dab42f6b0dd56a0cd2833d0dfff2
Carter Duffy <carterduffy4@gmail.com>
2025-02-04 11:54:26 -0500

    finished it

--- Print-Statments/Program.cs
+++ Print-Statments/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favMovie = "THE MATRIX";
+            
+            Console.WriteLine("Enter a name:");
+            var name = Console.ReadLine();
+            
+            Console.WriteLine("Hello " + name + ", your fav movie is " + favMovie);
+            Console.WriteLine();
+            
+            bool check = favMovie.Contains("THE");
+            Console.WriteLine("Var Contains 'THE': '" + check);
+            Console.WriteLine();
+
+            string replace = favMovie.Replace("A", "@").Replace("E", "3");
+            Console.WriteLine(replace);
         }
     }
 }
